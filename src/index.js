3a64e78ed31f48e8151a8039470ecd85e38837a5
Gerardo Gallegos <puxerop@gmail.com>
2019-01-11 12:28:38 -0600

    Leccion # 11 - Componentes de React con estado

--- src/index.js
+++ src/index.js
@@ -1,20 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const TarjetaFruta = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <h3>{ props.name }</h3>
-      <hr/>
-      <p>$ { props.price }</p>
-    </div>
-  )
+class TarjetaFruta extends React.Component {
+  constructor () {
+    super()
+
+    this.state = {
+      cantidad: 0
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <h3>{ this.props.name }</h3>
+        <div>Cantidad: { this.state.cantidad }</div>
+        <button
+          onClick={() => {
+            this.setState({
+              cantidad: this.state.cantidad + 1
+            })
+          }}
+        >
+          Agregar
+        </button>
+        <hr/>
+        <p>$ { this.props.price }</p>
+      </div>
+    )
+  }
 }
 
 const App = () => (
   <div>
-    <TarjetaFruta name={'Sandia'} price={2.00} />
+    <TarjetaFruta name={'Sandia'} price={5.00} />
     <TarjetaFruta name={'Naranja'} price={1.50} />
     <TarjetaFruta name='Kiwi' price={3.30} />
   </div>
